c58f6975741eb947e0477b80dd67d09b8c16d853
Binadam442 <sunusiadam442@gmail.com>
2024-12-07 15:29:55 -0800

    task_1/

---
+++ 0x01-react_intro/task_1/src/App.js
@@ -0,0 +1,25 @@
+import ALXLogo from './ALX-logo.jpg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={ALXLogo} alt="ALX-logo"></img>
+                <h1>School dashboard</h1>
+            </header>
+            <div className="App-body">
+                <p>
+                    Login to access the full dashboard
+                </p>
+            </div>
+            <div className="App-footer">
+                <p>
+                    Copyright 2020 - ALX School
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default App;
